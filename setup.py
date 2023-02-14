bf3059ae373415e0fe4a707f8b00864e5bb52d00
Sara Rajabi <s.rajabi@Notrika.local>
2023-02-14 16:21:55 +0330

    send email by smtplib -- edit setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(name='send_email',
       version='0.1',
       description='send_email by sara rajaei Demo Package',
-      url='#',
+      url='https://github.com/Sararajaei/send-email.git',
       author='Sara Rajaei',
       author_email='sararj9894@gmail.com',
       license='MIT',
